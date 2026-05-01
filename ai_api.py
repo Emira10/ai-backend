b96ade65cbb749adafd2d63306901f72755a3ad2
Amira SABBAGH <emira2000sabbagh@gmail.com>
2026-05-01 15:14:56 +0300

    add real efficientnet model

--- ai_api.py
+++ ai_api.py
@@ -1,20 +1,47 @@
 from fastapi import FastAPI, File, UploadFile
-import random
+from PIL import Image
+import torch
+import torch.nn as nn
+from torchvision import models, transforms
+import io
+import json
 
 app = FastAPI()
 
-classes = ["Eczema Photos", "Lichen Planus", "gül_hast", "mantar", "sedef"]
+with open("class_names.json", "r", encoding="utf-8") as f:
+    class_names = json.load(f)
+
+num_classes = len(class_names)
+
+model = models.efficientnet_b0(weights=None)
+in_features = model.classifier[1].in_features
+model.classifier[1] = nn.Linear(in_features, num_classes)
+
+state_dict = torch.load("psorai_efficientnetb0_weights.pth", map_location="cpu")
+model.load_state_dict(state_dict)
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
 
 @app.get("/")
 def home():
-    return {"message": "AI Backend çalışıyor"}
+    return {"message": "AI Backend gerçek model ile çalışıyor"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    prediction = random.choice(classes)
-    confidence = round(random.uniform(0.70, 0.95), 2)
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    image = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(image)
+        probs = torch.softmax(outputs, dim=1)
+        confidence, pred = torch.max(probs, 1)
 
     return {
-        "prediction": prediction,
-        "confidence": confidence
+        "prediction": class_names[pred.item()],
+        "confidence": float(confidence.item())
     }
